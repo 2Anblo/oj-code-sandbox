86806207da7c22b9a865998219304050c1f15ad9
Lingbo <1542988920@qq.com>
2026-03-08 15:21:42 +0800

    complete oj-code-sandbox in JavaDockerCodeSandbox part

--- src/main/java/com/lingbo/ojcodesandbox/JavaDockerCodeSandbox.java
+++ src/main/java/com/lingbo/ojcodesandbox/JavaDockerCodeSandbox.java
@@ -2,19 +2,24 @@ package com.lingbo.ojcodesandbox;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.dfa.FoundWord;
 import cn.hutool.dfa.WordTree;
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.*;
 import com.github.dockerjava.api.model.*;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.ExecStartResultCallback;
 import com.lingbo.ojcodesandbox.model.ExecuteCodeRequest;
 import com.lingbo.ojcodesandbox.model.ExecuteCodeResponse;
 import com.lingbo.ojcodesandbox.model.ExecuteMessage;
 import com.lingbo.ojcodesandbox.model.JudgeInfo;
 import com.lingbo.ojcodesandbox.utils.ProcessUtils;
+import org.springframework.util.StopWatch;
 
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -22,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class JavaDockerCodeSandbox implements CodeSandbox {
 
@@ -31,7 +37,7 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
 
     private static final Long TIME_OUT = 10000L;
 
-    private static final Boolean FIRST_INIT = true;
+    private static Boolean FIRST_INIT = true;
 
     public static void main(String[] args) {
         JavaDockerCodeSandbox codeSandbox = new JavaDockerCodeSandbox();
@@ -47,6 +53,8 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
 
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+
+
 
         // 1. 保存用户代码文件
         String code = executeCodeRequest.getCode();
@@ -95,6 +103,7 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
             try {
                 pullImageCmd.exec(pullImageResultCallback)
                         .awaitCompletion();
+                FIRST_INIT = false;
             } catch (InterruptedException e) {
                 System.out.println("拉取镜像异常");
                 throw new RuntimeException(e);
@@ -106,12 +115,15 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
         CreateContainerCmd containerCmd = dockerClient.createContainerCmd(image);
         HostConfig hostConfig = new HostConfig();
         hostConfig.withMemory(100 * 1000 * 1000L);
+        hostConfig.withMemorySwap(0L);
         hostConfig.withCpuCount(1L);
         hostConfig.setBinds(new Bind(userCodeSaveParentPath, new Volume("/app")));
 
         CreateContainerResponse createContainerResponse = containerCmd
                 .withHostConfig(hostConfig)
                 .withAttachStdout(true)
+                .withNetworkDisabled(true)
+                .withReadonlyRootfs(true)
                 .withAttachStderr(true)
                 .withAttachStdin(true)
                 .withTty(true)
@@ -128,11 +140,135 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
 
         // 启动容器
         dockerClient.startContainerCmd(containerId).exec();
+        List<ExecuteMessage> executeMessageList = new ArrayList<>();
+        // 执行代码，并获取结果
+        // docker exec keen_blackwell java -cp /app Main 1 3
+        // 取运行时间的最大值用于判断是否超时
+        long maxTime = 0;
+        for (String inputArgs : inputList) {
+            ExecuteMessage executeMessage = new ExecuteMessage();
+            long time = 0L;
 
+            final String[] message = {null};
+            final String[] errorMessage = {null};
+            final long[] maxMemory = {0L};
 
+            StopWatch stopWatch = new StopWatch();
+            StatsCmd statsCmd = dockerClient.statsCmd(containerId);
+            ResultCallback<Statistics> statisticsResultCallback = statsCmd.exec(
+                    new ResultCallback<Statistics>() {
+                        @Override
+                        public void onNext(Statistics statistics) {
+                            System.out.println("内存占用：" + statistics.getMemoryStats().getUsage() );
+                            maxMemory[0] = Math.max(statistics.getMemoryStats().getUsage(), maxMemory[0]);
+                        }
 
+                        @Override
+                        public void close() throws IOException {
+
+                        }
+
+                        @Override
+                        public void onStart(Closeable closeable) {
+
+                        }
+
+                        @Override
+                        public void onError(Throwable throwable) {
+
+                        }
+
+                        @Override
+                        public void onComplete() {
+
+                        }
+                    }
+            );
+            String[] inputArray = inputArgs.split(" ");
+            String [] cmdArray = ArrayUtil.append(new String[] {"java","-cp","/app","Main"} , inputArray);
+            ExecCreateCmdResponse cmdResponse = dockerClient.execCreateCmd(containerId)
+                    .withCmd(cmdArray)
+                    .withAttachStdin(true)
+                    .withAttachStdout(true)
+                    .withAttachStderr(true)
+                    .exec();
+            String execId = cmdResponse.getId();
+            final boolean[] isTimeOut = {true};
+            ExecStartResultCallback execStartResultCallback = new ExecStartResultCallback() {
+                @Override
+                public void onComplete() {
+                    super.onComplete();
+                    // 若程序执行完毕，则说明未超时
+                    isTimeOut[0] = false;
+                }
+
+                @Override
+                public void onNext(Frame frame) {
+                    StreamType streamType = frame.getStreamType();
+
+                    if (StreamType.STDERR.equals(streamType)) {
+                        errorMessage[0] = new String(frame.getPayload());
+
+                        System.out.println("输出错误结果："+ errorMessage[0]);
+                    }  else {
+                        message[0] = new String(frame.getPayload());
+
+                        System.out.println("输出结果"+ message[0]);
+                    }
+
+                    super.onNext(frame);
+                }
+            };
+            try {
+                stopWatch.start();
+                dockerClient.execStartCmd(execId)
+                        .exec(execStartResultCallback)
+                        .awaitCompletion(TIME_OUT, TimeUnit.MILLISECONDS);
+                stopWatch.stop();
+                time = stopWatch.getLastTaskTimeMillis();
+                executeMessage.setTime(time);
+                maxTime = Math.max(time,maxTime);
+                statsCmd.close();
+            } catch (InterruptedException e) {
+                System.out.println("程序执行异常");
+                throw new RuntimeException(e);
+            }
+            executeMessage.setErrorMessage(errorMessage[0]);
+            executeMessage.setMessage(message[0]);
+            executeMessageList.add(executeMessage);
+        }
+
+        // 4.整理输出
 
         ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        List<String> outputList = new ArrayList<>();
+        for (ExecuteMessage executeMessage : executeMessageList) {
+            String message = executeMessage.getMessage();
+            String errorMessage = executeMessage.getErrorMessage();
+            if (StrUtil.isNotBlank(errorMessage)) {
+                executeCodeResponse.setMessage(executeMessage.getErrorMessage());
+                // 执行中存在错误
+                executeCodeResponse.setStatus(3);
+                break;
+            }
+            outputList.add(message);
+        }
+        // 正常运行完成
+        if (outputList.size() == executeMessageList.size()) {
+            executeCodeResponse.setStatus(1);
+        }
+        executeCodeResponse.setOutputList(outputList);
+        JudgeInfo judgeInfo = new JudgeInfo();
+        judgeInfo.setTime(maxTime);
+
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+
+        // 5、文件清理
+        if (userCodeFile.getParentFile() != null) {
+            boolean del = FileUtil.del(userCodeSaveParentPath);
+            System.out.println("删除" + (del ? "成功" : "失败"));
+        }
+
         return executeCodeResponse;
     }
 
